2e2b806e040c99c3d3d519c791a6fc34af782030
Falk Schramm <falkschramm@gmail.com>
2023-11-12 11:00:53 +0100

    feat: new preprocessing with RecursiveCharacterTextSplitter

--- pages/config-preprocess-recursive.py
+++ pages/config-preprocess-recursive.py
@@ -1,27 +1,27 @@
 import streamlit as st
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
 #
-# preprocess-bsi.py
+# config-preprocess-recursive.py
 # Configure the preprocess object in session state.
-# In this case it is a CharacterTextSplitter.
-# You can configure, chunk_size and chunk_overlap.
+# In this case it is a RecursiveCharacterTextSplitter.
+# You can configure chunk_size and chunk_overlap.
 #
-def character_text_splitter_preprocessor(docs):
+def recursive_text_splitter_preprocessor(docs):
     # read the configuration for the CharacterTextSplitter from session state
     config_kwargs = st.session_state["preprocess_kwargs"]
     chunk_size = config_kwargs["chunk_size"]
     chunk_overlap = config_kwargs["chunk_overlap"]
     # configure the splitter, do the work and return results
-    text_splitter = CharacterTextSplitter(
+    text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size,
         chunk_overlap=chunk_overlap)
     docs_raw = text_splitter.split_documents(docs)
     return docs_raw
 
 
-st.write("# Configure CharacterTextSplitter preprocessing")
+st.write("# Configure RecursiveCharacterTextSplitter preprocessing")
 st.write("#### Current preprocessing")
 widget = st.text(f'Preprocessing name: {st.session_state["preprocess_name"]}')
 
@@ -33,12 +33,12 @@ new_kwargs["chunk_overlap"] = old_kwargs.get("chunk_overlap", 0)
 new_kwargs["keep_separator"] = old_kwargs.get("chunk_overlap", False)
 
 new_kwargs["chunk_size"] = st.number_input(label="Chunk size", value=new_kwargs["chunk_size"], min_value=1)
-new_kwargs["chunk_overlap"] = st.number_input(label="Chunk size", value=new_kwargs["chunk_overlap"], min_value=0)
+new_kwargs["chunk_overlap"] = st.number_input(label="Chunk overlap", value=new_kwargs["chunk_overlap"], min_value=0)
 
 
 if st.button("Set new preprocessing configuration"):
-    st.session_state["preprocess"] = character_text_splitter_preprocessor
-    st.session_state["preprocess_name"] = "CharacterTextSplitter"
+    st.session_state["preprocess"] = recursive_text_splitter_preprocessor
+    st.session_state["preprocess_name"] = "RecursiveCharacterTextSplitter"
     st.session_state["preprocess_kwargs"] = new_kwargs
     # update the preprocessing information at the top of the page
     widget.text(f'Preprocessing name: {st.session_state["preprocess_name"]}')
